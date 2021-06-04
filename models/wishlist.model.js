2ebd6a31f5c2069ffeacaa05d55077c498babc36
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-06-04 18:14:40 +0530

    update : wishlist model

--- models/wishlist.model.js
+++ models/wishlist.model.js
@@ -1,6 +1,7 @@
 const { Schema, model } = require("mongoose");
 
 const WishlistSchema = new Schema({
+  userId: { type: Schema.Types.ObjectId, ref: "User" },
   products: [{ type: Schema.Types.ObjectId, ref: "Product" }],
 });
 
