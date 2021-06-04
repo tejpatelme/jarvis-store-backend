88e541ed7de1dc9a3e502e245ad986b268f37995
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-06-04 18:14:24 +0530

    update : user model

--- models/user.model.js
+++ models/user.model.js
@@ -3,8 +3,6 @@ const { Schema, model } = require("mongoose");
 const UserSchema = new Schema({
   email: String,
   password: String,
-  cartId: Schema.Types.ObjectId,
-  wishlistId: Schema.Types.ObjectId,
 });
 
 const User = model("User", UserSchema);
