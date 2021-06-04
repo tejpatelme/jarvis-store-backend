ecbda8584dd0bb6b21166fdbe174761d66718f33
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-06-04 18:17:50 +0530

    update : use callbacks from users controller

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,39 +1,15 @@
 const express = require("express");
-const { User } = require("../models/user.model");
+const { signUpUser, logInUser } = require("../controllers/users.controller");
+const {
+  validateEmail,
+  validatePassword,
+  verifyToken,
+} = require("../middlewares");
 
 const router = express.Router();
 
-router.route("/signup").post(async (req, res) => {
-  const { email, password, cartId, wishlistId } = req.body;
+router.route("/signup").post(validateEmail, validatePassword, signUpUser);
 
-  try {
-    const user = await User.create({ email, password, cartId, wishlistId });
-    res.json(user);
-  } catch (err) {
-    res.status(500).json({ success: false, errorMessage: err.message });
-  }
-});
-
-router.route("/login").post(async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const user = await User.findOne({ email: email });
-    if (user) {
-      return user.password === password
-        ? res.status(200).json({ user })
-        : res.status(401).json({
-            success: false,
-            errorMessage: "Email and password don't match",
-          });
-    } else {
-      res.status(401).json({
-        success: false,
-        errorMessage: "Can't find an account with the email",
-      });
-    }
-  } catch (err) {
-    res.status(401).json({ success: false, errorMessage: err.message });
-  }
-});
+router.route("/login").post(logInUser);
 
 module.exports = router;
