d3afa62e91bcfcbf489091ef6c4002ea02c17c25
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-06-04 18:18:29 +0530

    update : use route not found, error handling middleware

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const {
   usersRouter,
   wishlistsRouter,
 } = require("./routes/index");
+const { routeNotFound, errorHandler } = require("./middlewares");
 
 const PORT = process.env.PORT || 3000;
 
@@ -25,5 +26,16 @@ app.use("/wishlist", wishlistsRouter);
 app.get("/", (req, res) =>
   res.send("API for Jarvis Store ecommerce application.")
 );
+
+/**
+ * 404 Router Handler.
+ * Do not move, this needs to be the last route.
+ */
+app.use(routeNotFound);
+
+/**
+ *Error Handler
+ */
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log("Server started on port", PORT));
