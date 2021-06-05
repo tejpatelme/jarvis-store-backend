b6f3315f4a43db4cf87ad3f5e5b499c7544bec84
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-06-05 10:22:12 +0530

    refactor : remove try/catch blocks

--- controllers/carts.controller.js
+++ controllers/carts.controller.js
@@ -3,101 +3,77 @@ const { Cart } = require("../models/cart.model");
 exports.getUsersCart = async (req, res) => {
   const { userId } = req;
 
-  try {
-    const cart = await Cart.findOne({ userId }).populate({
-      path: "products",
-      populate: {
-        path: "productId",
-      },
-    });
+  const cart = await Cart.findOne({ userId }).populate({
+    path: "products",
+    populate: {
+      path: "productId",
+    },
+  });
 
-    res.status(200).json({ success: true, cart });
-  } catch (err) {
-    res.status(500).json({ success: false, errorMessage: err.message });
-  }
+  res.status(200).json({ success: true, cart });
 };
 
 exports.putUsersCartInRequest = async (req, res, next, cartId) => {
-  try {
-    const cart = await Cart.findById(cartId);
-    req.cart = cart;
-    next();
-  } catch (err) {
-    res.status(500).json({ success: false, errorMessage: err.message });
-  }
+  const cart = await Cart.findById(cartId);
+  req.cart = cart;
+  next();
 };
 
 exports.addProductToCart = async (req, res) => {
   const { cart } = req;
   const { productId } = req.body;
 
-  try {
-    cart.products.push({
-      productId: productId,
-      qty: 1,
-    });
+  cart.products.push({
+    productId: productId,
+    qty: 1,
+  });
 
-    await cart.save();
+  await cart.save();
 
-    res.json({ success: true });
-  } catch (err) {
-    res.status(500).json({ success: false, errorMessage: err.message });
-  }
+  res.json({ success: true });
 };
 
 exports.increaseProductQuantity = async (req, res) => {
   const { cart } = req;
   const { productId } = req.body;
 
-  try {
-    const match = cart.products.findIndex(
-      (product) => String(product.productId) === productId
-    );
+  const match = cart.products.findIndex(
+    (product) => String(product.productId) === productId
+  );
 
-    cart.products[match].qty += 1;
+  cart.products[match].qty += 1;
 
-    const updatedCart = await cart.save();
+  const updatedCart = await cart.save();
 
-    res.status(200).json({ success: true });
-  } catch (err) {
-    res.status(500).json({ success: false, errorMessage: err.message });
-  }
+  res.status(200).json({ success: true });
 };
 
 exports.decreaseProductQuantity = async (req, res) => {
   const { cart } = req;
   const { productId } = req.body;
 
-  try {
-    const match = cart.products.findIndex(
-      (product) => String(product.productId) === productId
-    );
+  const match = cart.products.findIndex(
+    (product) => String(product.productId) === productId
+  );
 
-    cart.products[match].qty -= 1;
+  cart.products[match].qty -= 1;
 
-    const updatedCart = await cart.save();
+  const updatedCart = await cart.save();
 
-    res.status(200).json({ success: true });
-  } catch (err) {
-    res.status(500).json({ success: false, errorMessage: err.message });
-  }
+  res.status(200).json({ success: true });
 };
 
 exports.removeProductFromCart = async (req, res) => {
   const { cart } = req;
   const { productId } = req.body;
 
-  try {
-    const match = cart.products.findIndex(
-      (product) => String(product.productId) === productId
-    );
+  const match = cart.products.findIndex(
+    (product) => String(product.productId) === productId
+  );
 
-    cart.products.splice(match, 1);
+  cart.products.splice(match, 1);
 
-    const updatedCart = await cart.save();
+  const updatedCart = await cart.save();
 
-    res.status(200).json({ success: true });
-  } catch (err) {
-    res.status(500).json({ success: false, errorMessage: err.message });
-  }
+  res.status(200).json({ success: true });
 };
--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -7,73 +7,61 @@ const bcrypt = require("bcrypt");
 module.exports.checkIfUserExists = async (req, res, next) => {
   const { email } = req.body;
 
-  try {
-    const user = await User.findOne({ email: email.toLowerCase() });
+  const user = await User.findOne({ email: email.toLowerCase() });
 
-    if (user) {
-      return res.status(200).json({
-        success: false,
-        errorMessage:
-          "A user with the specified email already exists. Please login instead",
-      });
-    }
-
-    next();
-  } catch (err) {
-    res.status(500).json({ success: false, errorMessage: err.message });
+  if (user) {
+    return res.status(200).json({
+      success: false,
+      errorMessage:
+        "A user with the specified email already exists. Please login instead",
+    });
   }
+
+  next();
 };
 
 exports.signUpUser = async (req, res) => {
   const user = req.body;
 
-  try {
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(user.password, salt);
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
 
-    const newUser = await User.create({
-      email: user.email.toLowerCase(),
-      password: user.password,
-    });
+  const newUser = await User.create({
+    email: user.email.toLowerCase(),
+    password: user.password,
+  });
 
-    const userCart = await Cart.create({
-      userId: newUser._id,
-      products: [],
-    });
+  const userCart = await Cart.create({
+    userId: newUser._id,
+    products: [],
+  });
 
-    const userWishlist = await Wishlist.create({
-      userId: newUser._id,
-      products: [],
-    });
+  const userWishlist = await Wishlist.create({
+    userId: newUser._id,
+    products: [],
+  });
 
-    res.status(201).json({ success: true, newUser, userCart, userWishlist });
-  } catch (err) {
-    res.status(500).json({ success: false, errorMessage: err.message });
-  }
+  res.status(201).json({ success: true, newUser, userCart, userWishlist });
 };
 
 exports.logInUser = async (req, res) => {
   const { email, password } = req.body;
 
-  try {
-    const user = await User.findOne({ email });
+  const user = await User.findOne({ email });
 
-    if (user) {
-      const passwordValid = await bcrypt.compare(password, user.password);
+  if (user) {
+    const passwordValid = await bcrypt.compare(password, user.password);
 
-      if (passwordValid) {
-        const token = jwt.sign({ userId: user._id }, process.env.TOKEN_SECRET, {
-          expiresIn: "24h",
-        });
+    if (passwordValid) {
+      const token = jwt.sign({ userId: user._id }, process.env.TOKEN_SECRET, {
+        expiresIn: "24h",
+      });
 
-        return res.status(200).json({ success: true, token });
-      }
+      return res.status(200).json({ success: true, token });
     }
-
-    return res
-      .status(401)
-      .json({ success: false, errorMessage: "email or password is incorrect" });
-  } catch (err) {
-    res.status(500).json({ success: false, errorMessage: err.message });
   }
+
+  return res
+    .status(401)
+    .json({ success: false, errorMessage: "email or password is incorrect" });
 };
