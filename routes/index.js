7472b63e6001351ccc7f1fb2d4a6f4a9f2a41186
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-06-05 11:28:18 +0530

    update : change wishlists router filename

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const productsRouter = require("./products.router");
 const cartsRouter = require("./carts.router");
 const usersRouter = require("./users.router");
-const wishlistsRouter = require("./wishlist.router");
+const wishlistsRouter = require("./wishlists.router");
 
 module.exports = { productsRouter, cartsRouter, usersRouter, wishlistsRouter };
