5a39acf306181287a3274793b61e1922f1501923
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-09-06 10:03:50 +0530

    update : product model

--- models/product.model.js
+++ models/product.model.js
@@ -1,14 +1,43 @@
 const { Schema, model } = require("mongoose");
 
+// const ProductSchema = new Schema({
+//   name: String,
+//   brand: String,
+//   image: String,
+//   price: Number,
+//   inStock: Boolean,
+//   fastDelivery: Boolean,
+//   ratings: Number,
+//   color: String,
+// });
+
 const ProductSchema = new Schema({
-  name: String,
-  brand: String,
-  image: String,
-  price: Number,
-  inStock: Boolean,
-  fastDelivery: Boolean,
-  ratings: Number,
-  color: String,
+  name: {
+    type: String,
+    required: "Name of the product is required",
+  },
+  description: {
+    type: String,
+  },
+  category: {
+    type: String,
+    required: "Category of the product is required",
+  },
+  software: {
+    type: String,
+    required: "Software name is required",
+  },
+  imageURL: {
+    type: String,
+    required: "Image URL is required",
+  },
+  price: {
+    type: Number,
+    required: "Price of the product is required",
+  },
+  by: {
+    type: String,
+  },
 });
 
 const Product = model("Product", ProductSchema);
